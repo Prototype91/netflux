34911fef330e6d35568db28f05fdb959f7806fc6
Raze91 <hattab_sami@yahoo.fr>
2022-03-12 18:13:31 +0100

    add CastCard component

---
+++ src/components/CastCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <img :src="cast?.person?.image?.medium" :alt="cast.person.name" />
+    <p>
+      {{ cast.person.name }} <br />
+      as <br />{{ cast.character.name }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CastCard",
+  props: {
+    cast: {},
+  },
+};
+</script>
+
+<style scoped></style>

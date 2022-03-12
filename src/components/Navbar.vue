925aac46f12622ca23dd00c52f8fbde662a95648
Raze91 <hattab_sami@yahoo.fr>
2022-03-12 11:18:58 +0100

    remove unused components and views, add navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav>
+    <router-link to="/" class="netflux">Netflux</router-link>
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  align-items: center;
+  padding: 0 50px;
+  background-color: rgb(20, 20, 20);
+  margin: 0;
+}
+
+nav ul {
+  list-style: none;
+}
+
+nav a {
+  text-decoration: none;
+}
+
+nav ul a {
+  color: white;
+}
+
+nav ul a:hover {
+  color: #b3b3b3;
+}
+
+.netflux {
+  text-transform: uppercase;
+  color: red;
+  font-weight: 900;
+  letter-spacing: 2px;
+}
+</style>

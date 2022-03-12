e8cb89b2d97709a6cebc3eac5d04073e23b1ac84
Raze91 <hattab_sami@yahoo.fr>
2022-03-12 17:29:07 +0100

    add EpisodeCard component

---
+++ src/components/EpisodeCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <router-link class="card" :to="'/episode/' + episode.id">
+    <img :src="episode.image?.medium" :alt="episode.name" />
+    <p class="rating">{{ episode.rating.average }}</p>
+    <p>{{ episode.name }}</p>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "EpisodeCard",
+  props: {
+    episode: {},
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  position: relative;
+  text-decoration: none;
+  color: white;
+}
+
+.rating {
+  margin: 0;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  color: orange;
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 1px 2px;
+}
+</style>
